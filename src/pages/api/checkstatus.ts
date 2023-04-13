fa18102df4e7a5eb83d8dab564d19c10b6db5f1e
devsnowin <snowin.developer@gmail.com>
2023-04-13 20:10:07 +0530

    add status check for planetscale.

---
+++ src/pages/api/checkstatus.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	try {
+		await prisma.user.findMany();
+
+		const health = {
+			uptime: process.uptime(),
+			message: 'OK',
+			timestamps: Date.now(),
+		};
+
+		res.status(200).json(health);
+	} catch (e) {
+		const error = e as Error;
+
+		res.status(503).json({ message: error.message });
+	}
+}
